078c10a8a0a093e2a1e5111807192d3c670d3628
Kirill <Kirill@DESKTOP-291SP3A>
2024-05-08 17:11:05 +0300

    Добавьте файлы проекта.

---
+++ TZ4/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TZ4
+{
+    public class Numbers
+    {
+        public int[] DivOfNUm
+    }
+}
